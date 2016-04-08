b8c317dd6f58a43467b67d2425554d99599d21b3
Clément Dufour <clement_duf@hotmail.fr>
2016-04-08 12:23:38 +0200

    Mise à jour exemple pour utiliser cmd_motors

--- controleur/src/talker.cpp
+++ controleur/src/talker.cpp
@@ -1,5 +1,5 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "md49test/MotorCmd.h"
 
 #include <sstream>
 
@@ -20,33 +20,10 @@ int main(int argc, char **argv)
    */
   ros::init(argc, argv, "talker");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
+  ros::Publisher chatter_pub = n.advertise<md49test::MotorCmd>("cmdmotors", 1000);
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-
-  ros::Rate loop_rate(10);
+  ros::Rate loop_rate(1);
 
   /**
    * A count of how many messages we have sent. This is used to create
@@ -58,13 +35,10 @@ int main(int argc, char **argv)
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
-    std_msgs::String msg;
-
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-
-    ROS_INFO("%s", msg.data.c_str());
+    md49test::MotorCmd msg;
+    msg.speed1 = 5;
+    msg.speed1 = -5;
+    ROS_INFO("message envoyé");
 
     /**
      * The publish() function is how you send messages. The parameter
@@ -77,7 +51,6 @@ int main(int argc, char **argv)
     ros::spinOnce();
 
     loop_rate.sleep();
-    ++count;
   }
 
 
