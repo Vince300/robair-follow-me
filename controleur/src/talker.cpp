9c1f65dbc395366eb6ff46fb71995ede99fe0d9a
Clément Dufour <clement_duf@hotmail.fr>
2016-04-08 13:05:03 +0200

    Ajout commentaires exemple

--- controleur/src/talker.cpp
+++ controleur/src/talker.cpp
@@ -1,52 +1,34 @@
 #include "ros/ros.h"
+/*md49test est un package qui contient un fichier msg/MotorCmd.msg, 
+Il faut rajouter les dépendances dans package.xml et CMakeLists.txt 
+Elles permettent la création d'objet c++/python de ce type de message.*/
 #include "md49test/MotorCmd.h"
 
 #include <sstream>
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+/*Cette commande crée un noeud du nom de talker dans la package dans lequel il se trouve ( pour run , rosrun suivi talker )*/
   ros::init(argc, argv, "talker");
 
   ros::NodeHandle n;
+/*Cette commande crée un topic du nom cmdmotors et va publier un message de type md49test::MotorCmd dessus, 1000 messages dans le buffer*/
   ros::Publisher chatter_pub = n.advertise<md49test::MotorCmd>("cmdmotors", 1000);
-
+/*Nombre de messages par seconde*/
   ros::Rate loop_rate(1);
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
   int count = 0;
   while (ros::ok())
   {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
+    /*crée un objet md49test::MotorCmd et rempli ces attributs*/
     md49test::MotorCmd msg;
     msg.speed1 = 5;
     msg.speed1 = -5;
-    ROS_INFO("message envoyé");
+    
 
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
+    /*Publie le message sur le topic*/
     chatter_pub.publish(msg);
+    ROS_INFO("message envoyé");
 
     ros::spinOnce();
 
