826a60c96be50a6b972f585d3ba6dbb4739ae195
Nicolas Rocha <nicolas.rocha@ensimag.grenoble-inp.fr>
2016-04-15 14:41:38 +0200

    Mise en place subscriber/publisher dans l'exemple

--- controleur/src/talker.cpp
+++ controleur/src/talker.cpp
@@ -6,14 +6,22 @@ Elles permettent la création d'objet c++/python de ce type de message.*/
 
 #include <sstream>
 
+void chatterCallback(const md49test::MotorCmd::ConstPtr& msglis)
+{
+  ROS_INFO("I heard: [%s]", msglis->data.c_str());
+}
+
 int main(int argc, char **argv)
 {
 /*Cette commande crée un noeud du nom de talker dans la package dans lequel il se trouve ( pour run , rosrun suivi talker )*/
   ros::init(argc, argv, "talker");
+ros::init(argc, argv, "listener");
 
-  ros::NodeHandle n;
+  ros::NodeHandle nlis;
+ros::NodeHandle npub;
 /*Cette commande crée un topic du nom cmdmotors et va publier un message de type md49test::MotorCmd dessus, 1000 messages dans le buffer*/
-  ros::Publisher chatter_pub = n.advertise<md49test::MotorCmd>("cmdmotors", 1000);
+  ros::Publisher chatter_pub = npub.advertise<md49test::MotorCmd>("cmdmotors", 1000);
+ros::Subscriber sub = nlis.subscribe("chatter", 1000, chatterCallback);
 /*Nombre de messages par seconde*/
   ros::Rate loop_rate(1);
 
@@ -35,6 +43,6 @@ int main(int argc, char **argv)
     loop_rate.sleep();
   }
 
-
+ros::spin();
   return 0;
 }
